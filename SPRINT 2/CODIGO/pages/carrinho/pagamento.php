9cf6e8125c43393e7e0e4e28df4694d5852b15c8
Ana Araujo <anapro1141@gmail.com>
2020-11-09 13:50:44 -0300

    corrigido erro na pagina pagamento

--- SPRINT 2/CODIGO/pages/carrinho/pagamento.php
+++ SPRINT 2/CODIGO/pages/carrinho/pagamento.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Pagamento</title>
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+	<link rel="stylesheet" href="../../bootstrap-4.1.3-dist/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="../../styles/css/pagamento.css"/>
 	<link rel="stylesheet" type="text/css" href="../../styles/css/main.css"/>
 	<link rel="stylesheet" type="text/css" href="../../styles/css/fixed.css"/>
@@ -174,7 +174,6 @@
   </div>
 </div>
   
-
 <!-- Footer -->
 <footer>
 	<div id="contact" class="offset">
@@ -192,8 +191,8 @@
 		</div>
 </footer>
 
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+<script type="text/javascript" src="../../js/JQuery3.3.1.js"></script>
+<script type="text/javascript" src="../../js/pagamento.js"></script>
 
 </body>
 </html>
