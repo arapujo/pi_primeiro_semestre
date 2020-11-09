3e7f1c900d9503464a5c2c469c65a8cb708724c8
Vitor R. Lima <56142288+VilRL@users.noreply.github.com>
2020-11-09 16:55:30 -0300

    Add files via upload

--- SPRINT 2/CODIGO/pages/aluno/index.php
+++ SPRINT 2/CODIGO/pages/aluno/index.php
@@ -1,64 +1,30 @@
-<?php
-    include('../../action/conexão.php');
- session_start(); 
-?>
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="en">
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<title>Cybermind</title>
+<title>CyberMind</title>
 <link rel="stylesheet" href="../../styles/css/aluno.css">
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
 </head>
 <body>
-<div class="bs-menu">
-    <nav class="navbar navbar-expand-md navbar-light  menu-bar">
-        <a href="index.php" class="navbar-brand"><img class="logo-site" src="../../assets/img/logo.png"></a>
-        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div id="navbarCollapse" class="collapse navbar-collapse">
-            <ul class="nav navbar-nav">
-                <li class="nav-item">
-                    <a href="../carrinho/index.php" class="nav-link"><img class="icon-carrinho" src="../../assets/icon/shopping-cart-icon.png"></a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a href="aluno-lista-material.php" class="nav-link dropdown-toggle" data-toggle="dropdown">Materiais</a>
-                    <div class="dropdown-menu">
-                        <a href="index.php#curso" class="dropdown-item">Catálogo</a>
-                        <a href="#" class="dropdown-item">Fórum de dúvidas</a>
-                    </div>
-                </li>
-            </ul>
-            <ul class="nav navbar-nav ml-auto">
-                <li class="nav-item dropdown">
-                    <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">
-                   <img src="../../assets/icon/user.png"> <?php echo $_SESSION['usuario'] ?>
-                    </a>
-                    <div class="dropdown-menu dropdown-menu-right">
-                        <a href="../aluno/aluno-lista-material.php" class="dropdown-item">Ver materiais</a>
-                        <a href="perfil.php" class="dropdown-item">Ver perfil</a>
-                        <a href="historico.php" class="dropdown-item">Histórico de compras</a>
-                        <div class="dropdown-divider"></div>
-                        <a href="../../index.php"class="dropdown-item">Sair</a>
-                    </div>
-                </li>
-            </ul>
-        </div>
-    </nav>
-</div>
+
+<?php
+    include ('../../action/header.php')
+ ?>
 
 <!-- Material -->
 	
 <div class="card text-center">
  
   <div class="card-body">
-    <h5 class="card-title"></h5>
-    <p>Olá, <?php echo $_SESSION['nome'] ?>! Boas Vindas ao CyberMind!</p>
-    <a href="aluno-lista-material.php" class="btn btn-primary">Meu material</a>
+    <h5 class="card-title">Boas Vindas ao CyberMind!</h5>
+    <a href="aluno-material.php" class="btn btn-primary">Meu material</a>
   </div>
 </div>
 
@@ -81,7 +47,7 @@
                     <div class="card-desc">
 						<h3>Quiz Hardware</h3>
 						<br>
-                        <p>Teste os seus conhecimentos em hardware</p>
+                        <p>Teste os seus conhecimentos em hardware.</p>
                             <a href="../curso/logica.php" class="btn-card">Ver</a>   
                     </div>
                 </div>
@@ -226,10 +192,6 @@
 			&copy; CyberMind.
 		</div>
 </footer>
-
-<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
 </body>
 </html>                            
